f6e52310b156716d9ecde3783d1103600f136323
samuel-alves <samuel.f.alves@outlook.com>
2019-09-27 16:54:31 +0100

    [FIX] handle empty values as null

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ MyStream.prototype.writeLine = function(line) {
     if(!!that.quote && val != 'NULL' && !!val) {
       val = that.quote + val + that.quote;
     }
-    if(typeof that.replaceNullWith != 'undefined' && val == 'NULL') {
+    if(typeof that.replaceNullWith != 'undefined' && (val == 'NULL' || (val || "").trim() === '')) {
       val = this.replaceNullWith;
     }
     that._line.push(val);
